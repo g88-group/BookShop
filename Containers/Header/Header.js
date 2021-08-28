838a1e9745f4e9c9f65c23471485b6522887fafa
Ubaydulayev <ubaydullayevdastonbek@gmail.com>
2021-08-28 21:32:39 +0500

    doston

--- Containers/Header/Header.js
+++ Containers/Header/Header.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import HeaderWrapper from './HeaderWrapper'
+import React from "react";
+import HeaderWrapper from "./HeaderWrapper";
+import Image from "next/image";
+import banner from "../../img/banner.png";
 
 const Header = ({ children }) => {
-    return (
-        <HeaderWrapper>
-            <div className="header">
-                <img src="" alt="" />
-                <div>
-                    {children}
-                </div>
-            </div>
-        </HeaderWrapper>
-    )
-}
+  return (
+    <HeaderWrapper>
+      <div className="header">
+        <div>{children}</div>
+      <Image src={banner} alt="img" />
+      </div>
+    </HeaderWrapper>
+  );
+};
 
-export default Header
+export default Header;

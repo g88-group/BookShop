b4570eedc5289866aadc354da41ca8e3afde8d66
Sardor <sardorsafarov090@gmail.com>
2021-09-08 10:20:04 +0500

    m

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import { ThemeProvider } from "styled-components"
 import GlobalStyle, { darkTheme, lightTheme } from "../styles/GlobalsStyle"
 import Header from "../Containers/Header";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import { Provider } from "react-redux";
 import store from "../redux/store";
-import Router, { useRouter } from "next/router";
 import Head from "next/head"
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
@@ -16,13 +13,13 @@ Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState("light");
-
   const toggleTheme = () => {
     theme == 'light' ? setTheme('dark') : setTheme('light')
   }
   return <Provider store={store}>
     <Head>
       <script src="//code-eu1.jivosite.com/widget/3sNV9ryVBh" async></script>
+      <title>Bookshop</title>
     </Head>
     <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
       <GlobalStyle />
